7243a9d7b25de87ec64a21cd3adab57a36cc668d
Alex <68706402+7shep@users.noreply.github.com>
2023-02-08 14:58:12 -0500

    Add files via upload

---
+++ project.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import tkinter.font as tkFont
+from pygame import mixer
+import time
+from tkinter import filedialog
+from tkinter import *
+import os
+
+root = Tk()
+#Title at the top of the window
+root.title("Shep's Tech Flex")
+#Size of the screen
+root.geometry("485x700+290+10")
+#Sets the background colour to a nice pink
+root.config(background="#FFC1CC")
+
+root.mainloop()
