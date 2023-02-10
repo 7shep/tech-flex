b98f477c099b1aaf1e6ac9f911a5c0887c621e3d
Alex <68706402+7shep@users.noreply.github.com>
2023-02-09 22:38:36 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,10 +1,12 @@
-import tkinter as tk
-import tkinter.font as tkFont
 from pygame import mixer
 import time
 from tkinter import filedialog
 from tkinter import *
 import os
+
+def PlayMusic():
+    print('soon')
+
 
 root = Tk()
 #Title at the top of the window
@@ -12,6 +14,24 @@ root.title("Shep's Tech Flex")
 #Size of the screen
 root.geometry("485x700+290+10")
 #Sets the background colour to a nice pink
-root.config(background="#FFC1CC")
+root.configure(background="#FFC1CC")
+#Window can't be resized anymore. Helps because now the GUI wont be stretched/messed with during presentation
+root.resizable(False, False)
+mixer.init()
 
+lower_frame = Frame(root, bg = "#333333", width = "485", height = "200")
+lower_frame.place(x=0, y=500)
+
+playButton = PhotoImage(file = 'play.png')
+Button(root, image = playButton, bg = "#FFFFFF", height = 60, width = 60, command = PlayMusic).place(x=120, y= 500)
+
+pauseButton = PhotoImage(file = 'pause.png')
+Button(root, image = pauseButton, bg = "#FFFFFF", height = 60, width = 60, command = mixer.music.pause).place(x=300, y=500)
+
+stopButton = PhotoImage(file = 'stop.png')
+Button(root, image = pauseButton, bg = "#FFFFFF", height = 60, width = 60, command = mixer.music.stop).place(x=210, y = 600)
+
+
+
+#runs all the tkinter code
 root.mainloop()
