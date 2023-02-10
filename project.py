54d509d5f01d4b8f4f0f5b1268dd813e6153666b
Alex <68706402+7shep@users.noreply.github.com>
2023-02-10 18:37:41 -0500

    Update project.py

--- project.py
+++ project.py
@@ -5,12 +5,25 @@ from tkinter import filedialog
 from tkinter import *
 from tkinter.filedialog  import askopenfilename
 
-def PlayMusic():
-    print('soon')
+from pydub import AudioSegment
+from pydub.playback import play
 
-#for pausing and unpausing
+pygame.init
+pygame.mixer.init()
+
 num = 0
+multiplefiles = 0
 
+def PlayMusic():
+    global multiplefiles 
+    multiplefiles +=1
+    musicFile = askopenfilename(filetypes=[("Audio Files","*.wav")])
+    print(musicFile)
+    sound = pygame.mixer.Sound(musicFile)
+    sound.play()
+    if (multiplefiles %2) == 0:
+        print('test')
+        sound.stop()
 def PauseMusic():
     global num
     num += 1
@@ -18,18 +31,19 @@ def PauseMusic():
     #if music is playing
     if pygame.mixer.get_busy():
         if (num%2) == 0:
-            pygame.mixer.pause()
-            print("Music Paused!")
-        else: 
             pygame.mixer.unpause()
-            print("Music Unpaused!")
+            print("Music UnPaused!")
+        else: 
+            pygame.mixer.pause()
+            print("Music Paused")
     else:
         print("Music is not playing.")
 
-def BrowseMusic():
-    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-    print(filename)
+def StopMusic():
+    sound.stop()
         
+
+
 root = Tk()
 #Title at the top of the window
 root.title("Shep's Tech Flex")
@@ -39,22 +53,25 @@ root.geometry("485x700+290+10")
 root.configure(background="#FFC1CC")
 #Window can't be resized anymore. Helps because now the GUI wont be stretched/messed with during presentation
 root.resizable(False, False)
-mixer.init()
 
-lower_frame = Frame(root, bg = "#333333", width = "485", height = "200")
+lower_frame = Frame(root, bg = "#FFFFFF", width = "485", height = "200")
 lower_frame.place(x=0, y=500)
 
+#Play music button.
 playImage = PhotoImage(file =r"play1.png")
 Button(root, image=playImage, bg = "#FFFFFF", command=PlayMusic, height=60, width=60).place(x=120, y= 500)
 
+#Pause music button
 pauseButton = PhotoImage(file = 'pause1.png')
 Button(root, image = pauseButton, bg = "#FFFFFF", height = 60, width = 60, command = PauseMusic).place(x=300, y=500)
 
+#Stop button
 stopButton = PhotoImage(file = 'stop1.png')
-Button(root, image = stopButton, bg = "#FFFFFF", height = 60, width = 60, command = mixer.music.stop).place(x=120, y = 600)
+Button(root, image = stopButton, bg = "#FFFFFF", height = 60, width = 60, command = StopMusic).place(x=120, y = 600)
 
-browseFiles = PhotoImage(file = 'cloud.png')
-Button(root, image=browseFiles, bg = "#FFFFFF", height = 60, width = 60, command =BrowseMusic).place(x=300, y = 600)
+#Browse file button (might not need, added because I was on autopilot)
+#browseFiles = PhotoImage(file = 'gas.png')
+#Button(root, image=browseFiles, bg = "#FFFFFF", height = 60, width = 60, command =BrowseMusic).place(x=300, y = 600)
 
 #runs all the tkinter code
 root.mainloop()
